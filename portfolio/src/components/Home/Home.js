9481e4073fba401ab2faa643dbd299df10c01d6e
Hector Agudelo Carmona <hectoragudelo@Hectors-MacBook-Air.local>
2022-01-25 21:59:52 -0500

    built the home component with child components title, text, icons, background image

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,34 +1,86 @@
 import React from 'react';
-import { Container, Image, Row, Col } from 'react-bootstrap';
-import myself from '../../images/selfImage.jpeg';
+import styles from 'styled-components';
+import Img from '../../images/emile-perron-xrVDYZRGdw4-unsplash.jpeg';
+import Typewriter from 'typewriter-effect';
+import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs';
 import './Home.css';
+
+const Section = styles.section`
+
+display: flex;
+flex-direction: column;
+align-items: start;
+justify-content: center;
+background: linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.6)), url(${Img});;
+background-size: cover;
+height: 100vh;
+width: auto;
+
+`;
+
+const Title = styles.h1`
+margin: 2px 40px;
+color: #393E41;
+
+`;
+
+const Text = styles.h3`
+display: inline-flex;
+margin: 2px 40px;
+gap: 8px;
+color: #393E41 !important;
+`;
+
+const Container = styles.div`
+margin: 20px 40px;
+display: flex;
+flex-direction: row;
+align-items: start;
+justify-content:center;
+gap: 30px;
+`;
 
 export const Home = () => {
   return (
-    <Container
-      style={{ margin: '15% 0% auto 0%', padding: '50px' }}
-      fluid
-    >
-      <Row className='justify-content-center align-items-center'>
-        <Col sm='auto' md='6' lg='auto'>
-          <Container fluid>
-            <Image  src={myself} roundedCircle width='100%' height='100%'/>
-          </Container>
-        </Col>
-        <Col sm='12' md='6' lg='5'>
-          <p style={{ color: 'white', textAlign: 'justify', hyphens: 'auto' }}>
-            I am a very passionate, diligent and easygoing person that enjoys
-            team work and collaborative environments. I enjoy reading about
-            advancements in technology and space exploration. I consider myself
-            a Junior Front-End Web Developer eager to learn more and apply my
-            skills in a corporate setting. I have experience in building
-            web-applications in Vanilla JavaScript, Jquery, and Ajax. I also
-            have some knowledge of node.js and APIs integration. During my free
-            time I enjoy the outdoors. Connecting with nature has helped me
-            balance hours of computer training.
-          </p>
-        </Col>
-      </Row>
-    </Container>
+    <Section>
+      <Title>Hector Agudelo-Carmona</Title>
+      <Text>
+        I am a
+        <Typewriter
+          options={{
+            strings: [
+              'Frontend Developer',
+              'Software Engineer',
+              'Software Developer',
+            ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </Text>
+      <Container>
+        <a
+          href='https://www.linkedin.com/in/hector-agudelo-34aa62119/'
+          rel='noreferrer'
+          target='_blank'
+        >
+          <BsLinkedin className='icon' size={'1.5em'} />
+        </a>
+        <a
+          href='https://github.com/HectorAgudelo'
+          rel='noreferrer'
+          target='_blank'
+        >
+          <BsGithub className='icon' size={'1.5em'} />
+        </a>
+        <a
+          href='https://twitter.com/DevAgudelo'
+          rel='noreferrer'
+          target='_blank'
+        >
+          <BsTwitter className='icon' size={'1.5em'} />
+        </a>
+      </Container>
+    </Section>
   );
 };
--- portfolio/src/components/NavigationBar/NavigationBar.js
+++ portfolio/src/components/NavigationBar/NavigationBar.js
@@ -1,40 +1,49 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import './NavigationBar.css';
 
-
 export const NavigationBar = () => {
-  const [backgroundColor, setBackgroundColor] = useState(false)
+  const [backgroundColor, setBackgroundColor] = useState(false);
 
-  const changeBackground = () =>{
-    if(window.scrollY>=100){
-      setBackgroundColor(true)
-    }else{
-      setBackgroundColor(false)
+  const changeBackground = () => {
+    if (window.scrollY >= 100) {
+      setBackgroundColor(true);
+    } else {
+      setBackgroundColor(false);
     }
-  }
+  };
 
-  window.addEventListener('scroll', changeBackground)
+  window.addEventListener('scroll', changeBackground);
   return (
     <Navbar
       collapseOnSelect
       expand='lg'
       variant='light'
       fixed='top'
-      className={backgroundColor ? 'backgroundColor active': 'backgroundColor'}
+      className={backgroundColor ? 'backgroundColor active' : 'backgroundColor'}
     >
       <Container>
-        <Navbar.Brand className='navText' href='#home'>Hector Agudelo</Navbar.Brand>
+        <Navbar.Brand className='navText' href='#home'>
+          Hector Agudelo
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav
             className='justify-content-end'
             style={{ width: '100%', alignItems: 'center' }}
           >
-            <Nav.Link className='navText' href='#Home'>Home</Nav.Link>
-            <Nav.Link className='navText' href='#About'>About</Nav.Link>
-            <Nav.Link className='navText' href='#Work'>Work</Nav.Link>
-            <Nav.Link className='navText' href='#Contact'>Contact</Nav.Link>
+            <Nav.Link className='navText' href='#Home'>
+              Home
+            </Nav.Link>
+            <Nav.Link className='navText' href='#About'>
+              About
+            </Nav.Link>
+            <Nav.Link className='navText' href='#Work'>
+              Work
+            </Nav.Link>
+            <Nav.Link className='navText' href='#Contact'>
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
